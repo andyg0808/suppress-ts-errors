23d80bb7026fbbf42c96a763fa6f0620520c815d
Andrew Gilbert <andrewg800@gmail.com>
2024-08-06 14:37:03 -0700

    Remove node-html-parser dependency
    I don't want to do a review on the code of it, and I'm not needing the Vue support, so let's just ditch it for now.

--- src/lib/extractTypeScriptFromVue.ts
+++ src/lib/extractTypeScriptFromVue.ts
@@ -1,7 +1,6 @@
-import { parse } from "node-html-parser";
 
 export const extractTypeScriptFromVue = (source: string): string => {
-  const script = parse(source).querySelector('script[lang="ts"]');
+  const script = null;
 
   return script ? script.rawText : "";
 };
